b1217ef2e1b970a0af253b229b2b9f5c382e4305
Adrian Pikios <adrian@evokelabs.com>
2022-06-24 13:07:14 +1000

    Structure

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+// import { getStaticProps } from './page/[page_index]'
+// import BlogPage from './page/[page_index]'
+
+import Layout from '../../components/Layout'
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>Blog</h1>
+    </div>
+  )
+}
